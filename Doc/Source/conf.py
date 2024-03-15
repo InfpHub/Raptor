910d4b767e4adb08602f36dd545355f882667702
InfpPointPlus <infp@gmail.com>
2024-03-16 00:24:26 +0800

    初始提交

---
+++ Doc/Source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Raptor"
+copyright = "2024 凉州刺史"
+author = "https://github.com/InfpHub/Raptor"
+release = "1.0.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["Template"]
+exclude_patterns = []
+
+language = "zh_CN"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["Static"]
---
+++ Doc/Source/index.rst
@@ -0,0 +1,20 @@
+.. Raptor documentation master file, created by
+   sphinx-quickstart on Sun Mar 10 11:46:36 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Raptor's documentation!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
